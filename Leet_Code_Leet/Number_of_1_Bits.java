b44474c852d5d9f8801fe99fa17873d349ab6590
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-11-29 18:04:52 +0530

    Add files via upload

---
+++ Leet_Code_Leet/Number_of_1_Bits.java
@@ -0,0 +1,10 @@
+public class Number_of_1_Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+}
